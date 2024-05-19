cd045ac2ed2bbc91faebbacc505a790a864534a1
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-19 22:32:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const blockIdInput = document.getElementById("block_id");
+const colorInput = document.getElementById("colour_id");
+const changeButton = document.getElementById("change_button");
+const resetButton = document.getElementById("reset_button");
+const gridItems = document.querySelectorAll(".grid-item");
+
+changeButton.addEventListener("click",()=>{
+	let col = gridItems[blockIdInput.value-1]
+	if(col==undefined){
+		alert("block id not present!");
+	}
+	else{
+		col.style.backgroundColor = colorInput.value
+	}
+})
+resetButton.addEventListener("click",()=>{
+	for (let grid of gridItems) {
+		grid.style.backgroundColor = "transparent"
+	}
+})
